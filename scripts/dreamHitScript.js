20cb2729e85f48c1199e190a3a55387e0acf5f13
andymasteroffish <andymasteroffish@gmail.com>
2012-02-06 15:42:38 -0500

    Everything wiped. Putting in a new project with no sprite atlases. This has all of the unity files, not just the assets

--- scripts/dreamHitScript.js
+++
@@ -1,9 +0,0 @@
-var parent : Transform;	//the dream this goes with
-
-
-function OnTriggerEnter(other : Collider) {
-	var object= other.gameObject;
-	if (object.name=="player"){
-		parent.SendMessage("startTalk");
-	}
-}
